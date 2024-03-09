4fe2861f433db8ee800f6c30b73062dd40a280ce
moahammadApon <mohammadapon11.com>
2024-03-09 09:35:43 +0600

    section wrapper, font family and file structure expand

---
+++ src/app/(home)/page.jsx
@@ -0,0 +1,8 @@
+import SectionWrapper from "@/components/wrapper's/SectionWrapper";
+import React from "react";
+
+const HomePage = () => {
+  return <SectionWrapper>HomePage HomePage</SectionWrapper>;
+};
+
+export default HomePage;
---
+++ src/components/wrapper's/SectionWrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SectionWrapper = ({ children }) => {
+  return <div className="max-w-[1170px] mx-auto">{children}</div>;
+};
+
+export default SectionWrapper;
