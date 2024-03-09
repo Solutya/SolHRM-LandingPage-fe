58c9e9c00f6daa3e33686595a2d1d66c5338a26d
moahammadApon <mohammadapon11.com>
2024-03-09 11:53:48 +0600

    some color defined

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,8 +1,13 @@
+import Hero from "@/components/pages/home/Hero";
 import SectionWrapper from "@/components/wrapper's/SectionWrapper";
 import React from "react";
 
 const HomePage = () => {
-  return <SectionWrapper>HomePage HomePage</SectionWrapper>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default HomePage;
--- src/components/pages/home/NavBar.jsx
+++ src/components/pages/home/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return <div></div>;
 };
 
 export default NavBar;
