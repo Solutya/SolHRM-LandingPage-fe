3bb2a3fdbae29e9ed3540088bed1ef8214b33524
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-03-10 18:22:16 +0600

    price area section added+ image design hover effect added

--- src/components/pages/home/PriceArea.jsx
+++ src/components/pages/home/PriceArea.jsx
@@ -3,12 +3,23 @@ import React from "react";
 
 const PriceArea = () => {
   return (
-    <div>
-      <div>
-        <HeaderTitle
-          title={"Deliver End-To-End Retail Billing & Solutions"}
-          subTitle={"Choose a plan tailored to your needs"}
-        />
+    <div className="m-[5%] bg-[#f6f5fa] rounded-3xl">
+      <div className="p-[5%] ">
+        <div className="text-center">
+          <HeaderTitle
+            title={
+              <>
+                Deliver End-To-End Retail
+                <br /> Billing & Solutions
+              </>
+            }
+            subTitle={"Choose a plan tailored to your needs"}
+          />
+        </div>
+        {/* tabs section */}
+        <div>
+
+        </div>
       </div>
     </div>
   );
