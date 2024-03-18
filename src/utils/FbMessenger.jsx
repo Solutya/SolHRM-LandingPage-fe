e547d0678b1e2e0951ac064583149b49516ecab9
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-03-18 14:58:01 +0600

    chatbot set minimized

--- src/utils/FbMessenger.jsx
+++ src/utils/FbMessenger.jsx
@@ -5,7 +5,6 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 const FbMessenger = () => {
 // const fbAppId = process.env.FB_APPID;
 // const fbPageId = process.env.FB_PAGEID;
-// console.log(fbAppId,fbPageId)
   return (
     <FacebookProvider appId="441531811346225" chatSupport>
       <CustomChat pageId="120272619751630" minimized={true} />
