1bb3177b535ef4c55b54330b69fa0dcdefe4a271
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-03-27 12:56:06 +0600

    image type error fixed

--- src/data/FeaturesScrollData.js
+++ src/data/FeaturesScrollData.js
@@ -1,7 +1,7 @@
 import fe1 from "../../public/assets/images/features/dashboard.png";
 import fe2 from "../../public/assets/images/features/project.png";
 import fe3 from "../../public/assets/images/features/attendance.png";
-import fe4 from "../../public/assets/images/features/configs.PNG";
+import fe4 from "../../public/assets/images/features/configs.png";
 export const FeaturesScrollData = [
   {
     img1: fe1,
